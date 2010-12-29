13750d5b06f8023d3e9ca6f0cca9ee87b0e6edcd
nav <nav@ae133410-3351-999f-0c55-5ef6f7248681>
2010-12-29 19:15:33 +0000

    Add missing youtube.py file. Use sharded counters to keep track of votes. Still missing a lot of info.

--- appengine/model.py
+++ appengine/model.py
@@ -1,5 +1,8 @@
 """Datastore Model classes."""
 
+import random
+
+from google.appengine.api import memcache
 from google.appengine.ext import db
 
 
@@ -17,3 +20,37 @@ class Vote(db.Model):
   user = db.UserProperty(required=True)
   song = db.ReferenceProperty(Song, required=True)
   catalog = db.ReferenceProperty(MusicCatalog)
+
+
+class GeneralCounterShardConfig(db.Model):
+  name = db.StringProperty(required=True)
+  num_shards = db.IntegerProperty(required=True, default=5)
+
+
+class GeneralCounterShard(db.Model):
+  name = db.StringProperty(required=True)
+  count = db.IntegerProperty(required=True, default=0)
+
+
+def get_count(name):
+  total = memcache.get(name)
+  if total is None:
+    total = 0
+    for counter in GeneralCounterShard.all().filter('name = ', name):
+      total += counter.count
+    memcache.add(name, str(total), 60)
+  return total
+
+
+def increment(name):
+  config = GeneralCounterShardConfig.get_or_insert(name, name=name)
+  def txn():
+    index = random.randint(0, config.num_shards - 1)
+    shard_name = name + str(index)
+    counter = GeneralCounterShard.get_by_key_name(shard_name)
+    if counter is None:
+      counter = GeneralCounterShard(key_name=shard_name, name=name)
+    counter.count += 1
+    counter.put()
+  db.run_in_transaction(txn)
+  memcache.incr(name)
---
+++ appengine/youtube.py
@@ -0,0 +1,69 @@
+import logging
+import model
+import os
+
+from django.utils import simplejson
+
+import gdata.youtube
+import gdata.youtube.service
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class Main(webapp.RequestHandler):
+
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__),
+      'templates/youtube.html')
+    self.response.out.write(template.render(path, {}))
+
+
+class Search(webapp.RequestHandler):
+
+  def get(self):
+    query = self.request.get('q', None)
+    if query is None:
+      self.error(404)  # TODO(nav): Better error.
+    
+    yt_service = gdata.youtube.service.YouTubeService()
+    yt_query = gdata.youtube.service.YouTubeVideoQuery()
+    yt_query.vq = query
+    yt_query.orderby = 'relevance'
+    yt_query.racy = 'include'
+    yt_query.categories.append('/Music')
+    feed = yt_service.YouTubeQuery(yt_query)
+    
+    results = []
+    for entry in feed.entry:
+      title = entry.media.title.text
+      url = entry.GetSwfUrl()
+      thumbnails = [x.url for x in entry.media.thumbnail]
+      results.append({'title': title, 'url': url,
+                      'thumbnails': thumbnails})
+    self.response.out.write(simplejson.dumps(results))
+
+
+class Add(webapp.RequestHandler):
+
+  def get(self):
+    url = self.request.get('url', None)
+    if url is None:
+      self.error(404)  # TODO(nav): Better error.
+   
+    model.increment(url) 
+    self.response.out.write('OK')
+   
+
+application = webapp.WSGIApplication(
+  [('/youtube', Main),
+   ('/youtube/search', Search),
+   ('/youtube/add', Add)], debug=True)
+
+
+def main():
+  run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
