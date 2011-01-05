bb98a783c99d319f85d8e031012576aaf5caabda
nav <nav@ae133410-3351-999f-0c55-5ef6f7248681>
2011-01-05 20:35:36 +0000

    Remove old local client and jukebox stuff.

--- appengine/model.py
+++ appengine/model.py
@@ -6,22 +6,6 @@ from google.appengine.api import memcache
 from google.appengine.ext import db
 
 
-class MusicCatalog(db.Model):
-  name = db.StringProperty(required=True)
-  owner = db.UserProperty(required=True)
-
-
-class Song(db.Model):
-  filename = db.StringProperty(required=True)
-  catalog = db.ReferenceProperty(MusicCatalog)
-
-
-class Vote(db.Model):
-  user = db.UserProperty(required=True)
-  song = db.ReferenceProperty(Song, required=True)
-  catalog = db.ReferenceProperty(MusicCatalog)
-
-
 class Playlist(db.Model):
   owner = db.UserProperty(required=True)
 
