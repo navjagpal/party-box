e9fa02105680d83956985b8cf118e388f3e520fa
nav <nav@ae133410-3351-999f-0c55-5ef6f7248681>
2011-01-07 01:34:02 +0000

    Display playlist in editor using songBox style.

--- appengine/static/partybox.js
+++ appengine/static/partybox.js
@@ -10,3 +10,30 @@ function DoGetPlaylist(playlist, callback) {
   }
   req.send(null);
 }
+
+function AddSongbox(container, title, thumbnails) {
+  var clickWrapper = document.createElement('div');
+  clickWrapper.setAttribute('class', 'clickWrapper');
+  if (thumbnails.length) {
+    var thumbnail = document.createElement('div');
+    thumbnail.setAttribute('class', 'songThumb');
+    var image = document.createElement('img');
+    image.setAttribute('class', 'thumbnail');
+    image.setAttribute('src', thumbnails[0]);
+    thumbnail.appendChild(image);
+    clickWrapper.appendChild(thumbnail);
+  }
+  var songDetails = document.createElement('div');
+  songDetails.setAttribute('class', 'songDetails');
+  songDetails.appendChild(document.createTextNode(title));
+  clickWrapper.appendChild(songDetails);
+  var row = document.createElement('div');
+  row.setAttribute('class', 'songBox');
+  row.appendChild(clickWrapper);
+  container.appendChild(row);
+  var clearFix = document.createElement('div');
+  clearFix.setAttribute('class', 'clearfix');
+  clearFix.appendChild(document.createTextNode(' '));
+  row.appendChild(clearFix);
+}
+
